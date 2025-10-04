801d7bd25262ef8689ef963d17069a048efa4eda
AmrEmirate <amremirate03@gmail.com>
2025-10-04 21:21:10 +0700

    final deploy

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2020/api",
+  baseURL: "https://be-finpro-grandlounge.vercel.app/api",
   withCredentials: true, 
 });
 
