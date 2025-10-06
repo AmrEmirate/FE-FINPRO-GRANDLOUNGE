6bb4e96d5bfd3e10d5a283a04aba170ffe1231da
AmrEmirate <amremirate03@gmail.com>
2025-10-06 17:22:41 +0700

    deploy again

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2020/api",
+  baseURL: "https://grand-lounge-api.vercel.app/api",
   withCredentials: true, 
 });
 
